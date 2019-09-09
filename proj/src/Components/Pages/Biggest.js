b3602e892fd00249d8dfc781e722fc0bb7279cc5
kamilabartnik <kbartnik@g.pl>
2019-09-09 16:01:33 +0200

    different number of input in biggest

--- proj/src/Components/Pages/Biggest.js
+++ proj/src/Components/Pages/Biggest.js
@@ -1,20 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import BiggestItem from './Items/BiggestItem';
 
-class Biggest extends Component{
-  constructor(props){
+
+class Biggest extends Component {
+  constructor(props) {
     super(props);
-    this.state = { value: null}
+    this.state = { value: 2 }
   }
 
-handleChange = (e) => {
+  handleNumber = (event) => {
+    this.setState({ value: event.target.value });
+    console.log(this.state.value);
+  }
 
-}
+  render() {
 
-  render(){
-    return(
+    let table = [];
+
+    for(let i=0; i< this.state.value; i++) {
+        table.push(<BiggestItem key={i} /> )
+      }
+
+    return (
       <div>
         <p>How many numbers you want to pass?</p>
-        <input type="number" min="2" max="6" value={this.state.value} onChange={this.handleChange} />
+        <input type="number" min="2" max="6" defaultValue={this.state.value} onChange={this.handleNumber} />
+        <div>
+          {this.state.value > 1 ?
+            <span>{table}</span>
+            : null}
+        </div>
         <p>Największa liczba to: </p>
       </div>
     )
---
+++ proj/src/Components/Pages/Items/BiggestItem.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class BiggestItem extends Component{
+  render(){
+    return(
+      <div>
+        <p>Podaj liczbę:</p>
+        <input></input><br />
+      </div>
+    )
+  }
+}
+
+export default BiggestItem;
