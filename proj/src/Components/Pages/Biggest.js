d9793f4482699fa986ca7392d6e0e324fa56caaa
kamilabartnik <kbartnik@g.pl>
2019-09-11 10:41:03 +0200

    the last solution for biggest's input

--- proj/src/Components/Pages/Biggest.js
+++ proj/src/Components/Pages/Biggest.js
@@ -13,25 +13,21 @@ class Biggest extends Component {
   }
 
   handleNumber = (event) => {
-    this.state.value = event.target.value;
-    this.setState({value: event.target.value});
+    this.setState({value: event.target.value}, () => console.log(this.state.value));
     console.log(event.target.value);
-    console.log(this.state.value);
     this.table = []
-    for(let i=0; i< this.state.value; i++) {
-      this.table.push(<BiggestItem onBlur={(n) => this.big.push(n)} key={i} />)
-    }
-
   }
 
   render() {
 
-
+    for(let i=0; i< this.state.value; i++) {
+      this.table.push(<BiggestItem onBlur={(n) => this.big.push(n)} key={i} />)
+    }
 
     return (
       <div>
         <p>How many numbers you want to pass?</p>
-        <input type="number" min="2" max="6" onChange={this.handleNumber} />
+        <input type="number" min="2" max="6" onChange={this.handleNumber} defaultValue={this.state.value} />
         <div>
           {this.state.value > 1 ?
             <span>{this.table}</span>
