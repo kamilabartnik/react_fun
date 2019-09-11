daf0a6b302301bf8995348df63681c8b64539df9
kamilabartnik <kbartnik@g.pl>
2019-09-11 17:32:55 +0200

    parent child processing data

--- proj/src/Components/Pages/Dialog.js
+++ proj/src/Components/Pages/Dialog.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
+import DialogItem from './Items/DialogItem';
+
 
 
 class Dialog extends Component{
-  render(){
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: null,
+      forwardValue: null
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
+
+  handleClick() {
+    this.setState({ forwardValue: this.state.input });
+  }
+
+  render() {
     return(
       <div>
-        <dialog open>
-          <h1>Tytuł</h1>
-          <p>Treść</p>
-        </dialog>
+        <input onChange={this.handleChange} type="number"/>
+        <button onClick={this.handleClick}>Propagate to child</button>
+        <p>
+        {this.state.forwardValue && <DialogItem value={this.state.forwardValue} />}
+        </p>
       </div>
     )
   }
---
+++ proj/src/Components/Pages/Items/DialogItem.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class DialogItem extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fromParent: Number(props.value),
+      current: Number(props.value),
+    }
+
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.value !== this.state.fromParent) {
+      this.setState({
+        fromParent: Number(nextProps.value),
+        current: Number(nextProps.value),
+      })
+    }
+  }
+
+  handleIncrement() {
+    this.setState({ current: this.state.current + 1 })
+  }
+
+  handleDecrement() {
+    this.setState({ current: this.state.current - 1 })
+  }
+
+  render() {
+    return(
+      <div>
+        <label>{this.state.current}</label>
+        <button onClick={this.handleIncrement}>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+      </div>
+    );
+  }
+}
+
+
+export default DialogItem;
