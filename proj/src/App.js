f71575a6a9770ab9d0b18f92282522d36069cf7b
kamilabartnik <kbartnik@g.pl>
2019-10-15 11:24:59 +0200

    app on diet deploy

--- proj/src/App.js
+++ proj/src/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
-import { HashRouter, Route } from "react-router-dom";
 
 //CSS
 import './css/App.css';
 
 //Menu
 import NavBar from './Components/NavBar';
-
-//Pages
-import Table from './Components/Pages/table';
-import Name from './Components/Pages/name';
-import Randomizer from './Components/Pages/Randomizer';
-import StonePS from './Components/Pages/StonePS';
-import FileSelector from './Components/Pages/FileSelector';
-import Dialog from './Components/Pages/Dialog';
-import Biggest from './Components/Pages/Biggest';
-import Timer from './Components/Pages/Timer';
-import RickAndMorthy from './Components/Pages/RickAndMorthyCharactersCards';
-import StarWars from './Components/Pages/StarWars';
-import ToDoList from './Components/Pages/ToDoList';
-import Calc from './Components/Pages/Calc';
-import Race from './Components/Pages/Race';
-import ColorWheel from './Components/Pages/ColorWheel';
-import Charts from './Components/Pages/Charts';
-import TicTacToe from './Components/Pages/TicTacToe';
-import Search from './Components/Pages/Search';
+import RouterApp from './Components/RouterApp';
 
 
 class App extends Component {
@@ -52,28 +33,7 @@ class App extends Component {
             <NavBar />
           </div>
           <div className="pages">
-            <HashRouter basename="/">
-              <div>
-                <Route exact path='/' component={Name} />
-                <Route path='/sps' component={StonePS} />
-                <Route path='/biggest' component={Biggest} />
-                <Route path='/random-number' component={Randomizer} />
-                <Route path='/dialog' component={Dialog} />
-                <Route path='/hello-name' component={Name} />
-                <Route path='/even-number' component={Table} />
-                <Route path='/file-selector' component={FileSelector} />
-                <Route path='/timer' component={Timer} />
-                <Route path='/rick-and-morthy' component={RickAndMorthy} />
-                {/* <Route path='/star-wars' component={StarWars} /> */}
-                <Route path='/to-do-list' component={ToDoList} />
-                <Route path='/calc' component={Calc} />
-                <Route path='/race' component={Race} />
-                <Route path='/color-wheel' component={ColorWheel} />
-                <Route path='/chart' component={Charts} />
-                <Route path='/tic-tac-toe' component={TicTacToe} />
-                <Route path='/search' component={Search} />
-              </div>
-            </HashRouter>
+            <RouterApp />
             <div className="darkness">
               <button onClick={this.handleDarkOn}>{this.state.dark ? 'Light' : 'Dark'}</button>
             </div>
---
+++ proj/src/Components/RouterApp.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { HashRouter, Route } from "react-router-dom";
+
+//Pages
+import Table from './Pages/table';
+import Name from './Pages/name';
+import Randomizer from './Pages/Randomizer';
+import StonePS from './Pages/StonePS';
+import FileSelector from './Pages/FileSelector';
+import Dialog from './Pages/Dialog';
+import Biggest from './Pages/Biggest';
+import Timer from './Pages/Timer';
+import RickAndMorthy from './Pages/RickAndMorthyCharactersCards';
+import StarWars from './Pages/StarWars';
+import ToDoList from './Pages/ToDoList';
+import Calc from './Pages/Calc';
+import Race from './Pages/Race';
+import ColorWheel from './Pages/ColorWheel';
+import Charts from './Pages/Charts';
+import TicTacToe from './Pages/TicTacToe';
+import Search from './Pages/Search';
+
+
+class RouterApp extends Component {
+
+  render() {
+    return (
+        <div className="app">
+          <div className="pages">
+            <HashRouter basename="/">
+              <div>
+                <Route exact path='/' component={Name} />
+                <Route path='/sps' component={StonePS} />
+                <Route path='/biggest' component={Biggest} />
+                <Route path='/random-number' component={Randomizer} />
+                <Route path='/dialog' component={Dialog} />
+                <Route path='/hello-name' component={Name} />
+                <Route path='/even-number' component={Table} />
+                <Route path='/file-selector' component={FileSelector} />
+                <Route path='/timer' component={Timer} />
+                <Route path='/rick-and-morthy' component={RickAndMorthy} />
+                {/* <Route path='/star-wars' component={StarWars} /> */}
+                <Route path='/to-do-list' component={ToDoList} />
+                <Route path='/calc' component={Calc} />
+                <Route path='/race' component={Race} />
+                <Route path='/color-wheel' component={ColorWheel} />
+                <Route path='/chart' component={Charts} />
+                <Route path='/tic-tac-toe' component={TicTacToe} />
+                <Route path='/search' component={Search} />
+              </div>
+            </HashRouter>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RouterApp;
