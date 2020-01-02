960e659ce485ae4c9703d6e8cb0081e49c24d57c
kamilabartnik <kbartnik@g.pl>
2020-01-02 15:15:38 +0100

    deleting some descriptions

--- proj/src/Components/Pages/name.js
+++ proj/src/Components/Pages/name.js
@@ -30,19 +30,7 @@ class Name extends Component{
         </label>
         <br />
         <span id="hello">Hello {this.state.value}</span>
-        <div className="description">
-        <h2>Krótkim słówkiem wstępu: stronka złożyłam z rzeczy, których się uczyłam w międzyczasie,
-          także jest lekko bez sensu, ale posiada już pewne cechy, które są słabe i może z luzem wytknąć :D
-          Za wszelkie uwagi jestem bardzo wdzięczna.
-        </h2>
-        <h3>
-          Basic to mega proste rzeczy :)
-          <br />
-          API Fun to tylko ściągnięcie API :)
-          <br />
-          CSS Fun jest zabawą w CSS i też nie ma żadnego przesłania ;D
-        </h3>
-        </div>
+
       </div>
     );
   }
